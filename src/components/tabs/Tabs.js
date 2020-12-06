e58bc19a4e2ed0c06f72af638eb96d1a6334a909
erwanriou <rr.1.riou@gmail.com>
2020-12-06 04:52:04 -0600

    [Project] - add cypress and update location folder

--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react"
 
-// LOAD COMPONENTS
+// LOAD COMPONENTS && STYLING
 import Tab from "./Tab"
-
-// LOAD STYLING
 import { useStyles } from "../../styles/tabs.styles.js"
 
 const Tabs = () => {
@@ -12,7 +10,7 @@ const Tabs = () => {
   const [tab, setTab] = useState({ index: 1, content: "Content of tab 1" })
 
   // HANDLE FUNCTIONS
-  const handleTabSelect = n => setTab({ index: n, content: `Content of tab ${n}` })
+  const handleTabSelect = n => setTab({ index: n + 1, content: `Content of tab ${n + 1}` })
   const handleStyles = n => {
     const isSelect = tab.index === n + 1 && "#0095f2"
     return { color: isSelect, borderColor: isSelect }
@@ -20,7 +18,7 @@ const Tabs = () => {
 
   // RENDERS
   const TabNav = [...Array(3).keys()].map(n => (
-    <h3 className={classes.tabWrapper} style={handleStyles(n, "borderColor")} onClick={() => handleTabSelect(n + 1)}>{`Tab${n + 1}`}</h3>
+    <h3 className={classes.tabWrapper} style={handleStyles(n)} onClick={() => handleTabSelect(n)}>{`Tab${n + 1}`}</h3>
   ))
   return (
     <div className={classes.tabContainer}>
