b762c6addb61d54697f574a7ae25507e843ee146
erwanriou <rr.1.riou@gmail.com>
2020-12-06 03:41:02 -0600

    [Initiate] - starting cleaning up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Tab, Tabs } from './Tabs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tabs>
+      <Tab title='Tab1'>Content of tab 1</Tab>
+      <Tab title='Tab2'>Content of tab 2</Tab>
+      <Tab title='Tab3'>Content of tab 3</Tab>
+  </Tabs>
   );
 }
 
